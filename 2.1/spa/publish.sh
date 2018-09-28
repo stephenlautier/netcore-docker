a033c16263a9b1b132ee730897cbb857846d6ad1
Stephen Lautier <stephen.lautier@CPM.lan>
2018-09-28 14:09:57 +0200

    feat(spa): update .net docker image

--- 2.1/spa/publish.sh
+++ 2.1/spa/publish.sh
@@ -1,6 +1,6 @@
-VERSION=2.1.0
+VERSION=2.1.4
 IMAGE=stephenlautier/netcore-docker-spa:${VERSION}
 echo "Building + pushing image '${IMAGE}'"
 
-docker build -t ${IMAGE} .
+docker build -t ${IMAGE} . #--no-cache --pull
 docker push ${IMAGE}
